907ef88229ea6eb4a762e622c90682f5b8e45eae
jihee99 <oosoojh31@gmail.com>
2024-04-22 22:59:07 +0900

    feat: db 연결 정보 수정

--- src/main/java/com/ex/ticket/GroupInitializer.java
+++ src/main/java/com/ex/ticket/GroupInitializer.java
@@ -1,48 +1,48 @@
-package com.ex.ticket;
-
-import com.ex.ticket.group.domain.entity.Group;
-import com.ex.ticket.group.domain.entity.GroupUser;
-import com.ex.ticket.group.domain.entity.GroupUserRole;
-import com.ex.ticket.group.repository.GroupRepository;
-import com.ex.ticket.group.service.CreateGroupUseCase;
-import com.ex.ticket.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
-
-import java.util.Set;
-
-@RequiredArgsConstructor
-@Component
-public class GroupInitializer implements ApplicationRunner {
-
-	private final GroupRepository groupRepository;
-	private final UserRepository userRepository;
-
-	private final CreateGroupUseCase createGroupUseCase;
-	@Override
-	public void run(ApplicationArguments args) {
-
-		Group group = Group.builder()
-		   .name("테스트 그룹")
-		   .introduce("테스트용 그룹입니다.")
-		   .contactEmail("master1@test.com")
-		   .contactNumber("010-1234-1234")
-		   .masterUserId(3L)
-		   .build();
-
-
-		groupRepository.save(group);
-		final GroupUser masterGroupUser = toMasterGroupUser(group, 3L);
-		group.addGroupUsers(Set.of(masterGroupUser));
-		group.getGroupUsers().forEach(GroupUser::activate);
-		groupRepository.save(group);
-
-	}
-
-	public GroupUser toMasterGroupUser(Group group, Long userId) {
-		return GroupUser.builder().userId(userId).group(group).role(GroupUserRole.MASTER).build();
-	}
-
-}
+// package com.ex.ticket;
+//
+// import com.ex.ticket.group.domain.entity.Group;
+// import com.ex.ticket.group.domain.entity.GroupUser;
+// import com.ex.ticket.group.domain.entity.GroupUserRole;
+// import com.ex.ticket.group.repository.GroupRepository;
+// import com.ex.ticket.group.service.CreateGroupUseCase;
+// import com.ex.ticket.user.repository.UserRepository;
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.boot.ApplicationArguments;
+// import org.springframework.boot.ApplicationRunner;
+// import org.springframework.stereotype.Component;
+//
+// import java.util.Set;
+//
+// @RequiredArgsConstructor
+// @Component
+// public class GroupInitializer implements ApplicationRunner {
+//
+// 	private final GroupRepository groupRepository;
+// 	private final UserRepository userRepository;
+//
+// 	private final CreateGroupUseCase createGroupUseCase;
+// 	@Override
+// 	public void run(ApplicationArguments args) {
+//
+// 		Group group = Group.builder()
+// 		   .name("테스트 그룹")
+// 		   .introduce("테스트용 그룹입니다.")
+// 		   .contactEmail("master1@test.com")
+// 		   .contactNumber("010-1234-1234")
+// 		   .masterUserId(3L)
+// 		   .build();
+//
+//
+// 		groupRepository.save(group);
+// 		final GroupUser masterGroupUser = toMasterGroupUser(group, 3L);
+// 		group.addGroupUsers(Set.of(masterGroupUser));
+// 		group.getGroupUsers().forEach(GroupUser::activate);
+// 		groupRepository.save(group);
+//
+// 	}
+//
+// 	public GroupUser toMasterGroupUser(Group group, Long userId) {
+// 		return GroupUser.builder().userId(userId).group(group).role(GroupUserRole.MASTER).build();
+// 	}
+//
+// }
--- src/main/java/com/ex/ticket/UserInitializer.java
+++ src/main/java/com/ex/ticket/UserInitializer.java
@@ -1,70 +1,70 @@
-package com.ex.ticket;
-
-import java.time.LocalDateTime;
-
-
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-
-import com.ex.ticket.user.domain.entity.AccountRole;
-import com.ex.ticket.user.domain.entity.AccountState;
-import com.ex.ticket.user.domain.entity.User;
-import com.ex.ticket.user.repository.UserRepository;
-
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-@Component
-public class UserInitializer implements ApplicationRunner {
-
-	private final UserRepository userRepository;
-	private final PasswordEncoder passwordEncoder;
-
-   @Override
-   public void run(ApplicationArguments args) {
-
-		User user = User.builder()
-			// .username("test1")test1
-			.email("user1@test.com")
-			.password(passwordEncoder.encode("1234"))
-			.name("테스트사용자1")
-			.accountRole(AccountRole.USER)
-			.accountState(AccountState.NORMAL)
-			.build();
-
-	   User manager = User.builder()
-		   // .username("test1")test1
-		   .email("manager1@test.com")
-		   .password(passwordEncoder.encode("1234"))
-		   .name("테스트매니저1")
-		   .accountRole(AccountRole.MANAGER)
-		   .accountState(AccountState.NORMAL)
-		   .build();
-
-	   User master = User.builder()
-		   // .username("test1")test1
-		   .email("master1@test.com")
-		   .password(passwordEncoder.encode("1234"))
-		   .name("테스트마스터1")
-		   .accountRole(AccountRole.MASTER)
-		   .accountState(AccountState.NORMAL)
-		   .build();
-
-	   User admin = User.builder()
-		   // .username("test1")test1
-		   .email("admin@system.com")
-		   .password(passwordEncoder.encode("1234"))
-		   .name("관리자")
-		   .accountRole(AccountRole.ADMIN)
-		   .accountState(AccountState.NORMAL)
-		   .build();
-
-	   userRepository.save(user);
-	   userRepository.save(manager);
-	   userRepository.save(master);
-	   userRepository.save(admin);
-
-   }
-}
+// package com.ex.ticket;
+//
+// import java.time.LocalDateTime;
+//
+//
+// import org.springframework.boot.ApplicationArguments;
+// import org.springframework.boot.ApplicationRunner;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.stereotype.Component;
+//
+// import com.ex.ticket.user.domain.entity.AccountRole;
+// import com.ex.ticket.user.domain.entity.AccountState;
+// import com.ex.ticket.user.domain.entity.User;
+// import com.ex.ticket.user.repository.UserRepository;
+//
+// import lombok.RequiredArgsConstructor;
+//
+// @RequiredArgsConstructor
+// @Component
+// public class UserInitializer implements ApplicationRunner {
+//
+// 	private final UserRepository userRepository;
+// 	private final PasswordEncoder passwordEncoder;
+//
+//    @Override
+//    public void run(ApplicationArguments args) {
+//
+// 		User user = User.builder()
+// 			// .username("test1")test1
+// 			.email("user1@test.com")
+// 			.password(passwordEncoder.encode("1234"))
+// 			.name("테스트사용자1")
+// 			.accountRole(AccountRole.USER)
+// 			.accountState(AccountState.NORMAL)
+// 			.build();
+//
+// 	   User manager = User.builder()
+// 		   // .username("test1")test1
+// 		   .email("manager1@test.com")
+// 		   .password(passwordEncoder.encode("1234"))
+// 		   .name("테스트매니저1")
+// 		   .accountRole(AccountRole.MANAGER)
+// 		   .accountState(AccountState.NORMAL)
+// 		   .build();
+//
+// 	   User master = User.builder()
+// 		   // .username("test1")test1
+// 		   .email("master1@test.com")
+// 		   .password(passwordEncoder.encode("1234"))
+// 		   .name("테스트마스터1")
+// 		   .accountRole(AccountRole.MASTER)
+// 		   .accountState(AccountState.NORMAL)
+// 		   .build();
+//
+// 	   User admin = User.builder()
+// 		   // .username("test1")test1
+// 		   .email("admin@system.com")
+// 		   .password(passwordEncoder.encode("1234"))
+// 		   .name("관리자")
+// 		   .accountRole(AccountRole.ADMIN)
+// 		   .accountState(AccountState.NORMAL)
+// 		   .build();
+//
+// 	   userRepository.save(user);
+// 	   userRepository.save(manager);
+// 	   userRepository.save(master);
+// 	   userRepository.save(admin);
+//
+//    }
+// }
