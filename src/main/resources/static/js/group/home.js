bb4e85d86c39e74a87353d154caf906ca68855bf
손지희 <wlhee@ejbt.co.kr>
2024-04-29 20:20:29 +0900

    feat: 그룹 정보 관리 화면 추가

--- src/main/resources/static/js/group/home.js
+++ src/main/resources/static/js/group/home.js
@@ -1,34 +1,28 @@
 (function($) {
-    $(document).ready(function() {
-        // 페이지가 로드되면 실행할 코드를 여기에 작성합니다.
-        // 예를 들어, 서버에 AJAX 요청을 보내는 코드:
-        $.ajax({
-            url: '/api/group/manage',
-            method: 'GET',
-            success: function(response) {
-                console.log(response)
-                console.log('요청이 성공적으로 완료되었습니다.');
 
-                let innerHtml = '';
-                response.forEach(function(item) {
-                    console.log(item)
-                    innerHtml += `
-                        <div class="card mb-4">
-                            <div class="card-body">
-                                <p class="mb-0">
-                                    <h5 class="fw-bold" value="${item.groupId}">${item.name}</h5>
-                                    <span>${item.introduce}</span>
-                                </p>
-                                <a href="/api/group/${item.groupId}/home">바로가기</a>
-                            </div>
-                        </div>`;
-                });
-                $('#list').html(innerHtml)
-                // 요소 만들어서 넣기
+    const $groupId = document.getElementById('groupIdContainer').getAttribute('data-group-id');
+
+    $(document).ready(function() {
+        $.ajax({
+            type: "GET",
+            url: `/api/group/manage/${$groupId}`,
+            success: function(response){
+                console.log("요청이 성공했습니다.");
+                console.log("서버로부터의 응답: ", response);
+
+                // 받은 응답을 사용하여 폼 요소에 값을 설정합니다.
+                $('#name').val(response.name);
+                $('#introduce').val(response.introduce);
+                $('#contactEmail').val(response.contactEmail);
+                $('#contactNumber').val(response.contactNumber);
+                $('#email').val(response.email);
+                $('#email').val(response.masterUser.email);
             },
-            error: function(xhr, status, error) {
-                console.error('요청이 실패했습니다:', status, error);
+            error: function(xhr, status, error){
+                console.error("요청이 실패했습니다.");
+                console.error("에러 내용: ", error);
+                alert("전시 상태를 확인하세요");
             }
-        });
+        })
     });
 })(jQuery);
